52f906256a33f346e5a373abc1213c0afaa44394
achibhossengit <mail.achibhossen@gmail.com>
2025-05-23 22:20:37 +0600

    fix toggle error by using optional Optional Chaining Operator

--- js/toggle.js
+++ js/toggle.js
@@ -1,13 +1,11 @@
-document.getElementById("discover-cart").addEventListener("click", function () {
+document.getElementById("discover-cart")?.addEventListener("click", function () {
   window.location.href = "question.html"
 });
 
 
 document
   .getElementById("back-to-desk-btn")
-  .addEventListener("click", function () {
+  ?.addEventListener("click", function () {
     window.location.href = "index.html";
     console.log('hello coder');
   });
-
-// just work which one is upper. both of them is not working. but why???????
